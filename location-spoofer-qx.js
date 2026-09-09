3a0d3daa14d431feedf5b912861769e635e963c7
NewObject <44203611+lj991213@users.noreply.github.com>
2026-09-09 15:16:07 +0800

    feat

--- location-spoofer-qx.js
+++ location-spoofer-qx.js
@@ -7,8 +7,8 @@
 
   var DEFAULT_CONFIG = {
     enabled: true,
-    latitude: 28.1125,
-    longitude: 113.01377,
+    latitude: 28.11140,
+    longitude: 113.01588,
     horizontalAccuracy: 39,
     verticalAccuracy: 1000,
     altitude: 530,
